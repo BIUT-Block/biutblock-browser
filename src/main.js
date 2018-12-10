90783b0901ee1509d7b4cac830d9caf38b054a15
Zu Weihan <zuweihan1992@gmail.com>
2018-12-10 15:08:14 +0100

    fix

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 const SECCORE = require('@sec-block/secjs-core')
 const SECRPC = require('@sec-block/secjs-rpc')
 
-exports.secCore = SECCORE.core
-exports.secRpc = new SECRPC(exports.secCore)
+const secCore = new SECCORE()
+const secRpc = new SECRPC(secCore)
 
 exports.run = function () {
-  SECCORE.run()
-  exports.secRpc.runRPCServer()
+  secCore.run()
+  secRpc.runRPCServer()
 }
