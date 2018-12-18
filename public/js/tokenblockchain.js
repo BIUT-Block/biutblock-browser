93bfab8af3a4bc10fec1d701f7f9b172c50ccce4
abysmli <abysmli@gmail.com>
2018-12-18 04:01:45 +0100

    Fix Bugs

--- public/js/tokenblockchain.js
+++ public/js/tokenblockchain.js
@@ -10,7 +10,7 @@ $('#pagination').pagination({
     blockchain.forEach((block) => {
       $('#blockchain-table').append(`
         <tr>
-          <td><a href="/tokenblockdetails?hash=${block.Hash}">${block.Number + 1}</a></td>
+          <td><a href="/tokenblockdetails?hash=${block.Hash}">${block.Number}</a></td>
           <td><a href="/tokenblockdetails?hash=${block.Hash}">0x${block.Hash}</a></td>
           <td>${block.Transactions.length}</td>
           <td>0x${block.Beneficiary}</td>
