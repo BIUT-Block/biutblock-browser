48fef35e4b26f3eb3b7184482a71a4422b4a3319
abysmli <abysmli@gmail.com>
2018-12-29 01:07:22 +0100

    Add: Guides

--- public/js/accountdetails.js
+++ public/js/accountdetails.js
@@ -2,18 +2,17 @@
 $(function () {
   $('#copyBtn').on('click', function () {
     let text = document.getElementById('address')
-    // if (document.body.createTextRange) {
-    //   let range = document.body.createTextRange()
-    //   range.moveToElementText(text)
-    //   range.select()
-    // } else if (window.getSelection) {
-    //   let selection = window.getSelection()
-    //   let range = document.createRange()
-    //   range.selectNodeContents(text)
-    //   selection.removeAllRanges()
-    //   selection.addRange(range)
-    // }
-    text.select()
+    if (document.body.createTextRange) {
+      let range = document.body.createTextRange()
+      range.moveToElementText(text)
+      range.select()
+    } else if (window.getSelection) {
+      let selection = window.getSelection()
+      let range = document.createRange()
+      range.selectNodeContents(text)
+      selection.removeAllRanges()
+      selection.addRange(range)
+    }
     document.execCommand('copy')
     $('#copyBtn').html('Copied').addClass('copyActive')
   })
