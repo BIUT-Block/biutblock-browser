245bd53fd111a10aef0847bcd2431c6c3fd44e70
abysmli <abysmli@gmail.com>
2018-12-29 01:03:57 +0100

    Add: Guides
    Former-commit-id: 77030b124f914ea7e5c1470646f148457974a513

--- public/js/accountdetails.js
+++ public/js/accountdetails.js
@@ -2,17 +2,19 @@
 $(function () {
   $('#copyBtn').on('click', function () {
     let text = document.getElementById('address')
-    if (document.body.createTextRange) {
-      let range = document.body.createTextRange()
-      range.moveToElementText(text)
-      range.select()
-    } else if (window.getSelection) {
-      let selection = window.getSelection()
-      let range = document.createRange()
-      range.selectNodeContents(text)
-      selection.removeAllRanges()
-      selection.addRange(range)
-    }
+    // if (document.body.createTextRange) {
+    //   let range = document.body.createTextRange()
+    //   range.moveToElementText(text)
+    //   range.select()
+    // } else if (window.getSelection) {
+    //   let selection = window.getSelection()
+    //   let range = document.createRange()
+    //   range.selectNodeContents(text)
+    //   selection.removeAllRanges()
+    //   selection.addRange(range)
+    // }
+    text.select()
+    document.execCommand('copy')
     $('#copyBtn').html('Copied').addClass('copyActive')
   })
 })
