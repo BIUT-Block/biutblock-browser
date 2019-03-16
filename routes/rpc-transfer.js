afbd4c410a53bae2d24f39150c304c7295c28be5
abysmli <abysmli@gmail.com>
2019-03-16 12:51:39 +0100

    Add: new files

---
+++ routes/rpc-transfer.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const router = express.Router()
+const request = require('request')
+
+/* GET home page. */
+const jsonrpc = '2.0'
+const rpcid = 1
+
+router.post('/callrpc', (req, res, next) => {
+  let bodyRequest = {
+    'method': req.body.method,
+    'jsonrpc': jsonrpc,
+    'id': rpcid,
+    'params': req.body.params
+  }
+  request({
+    method: 'POST',
+    url: 'http://localhost:3002',
+    body: JSON.stringify(bodyRequest),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }, (err, response, body) => {
+    if (err) {
+      res.json(err)
+    }
+    res.json(response)
+  })
+})
+
+module.exports = router
