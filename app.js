326148c491067bb6e3a51b5254e6f20dc0161bde
abysmli <abysmli@gmail.com>
2019-03-16 12:47:25 +0100

    Update new data
    Former-commit-id: 92268232101bd1294ecb6b222e692caa1fe2d8e8

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
-let createError = require('http-errors')
-let express = require('express')
-let partials = require('express-partials')
-let path = require('path')
-let cookieParser = require('cookie-parser')
-let logger = require('morgan')
-let indexRouter = require('./routes/index')
+const createError = require('http-errors')
+const express = require('express')
+const partials = require('express-partials')
+const cors = require('cors')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const indexRouter = require('./routes/index')
+const RPCTransfer = require('./routes/rpc-transfer')
 
 let app = express()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(partials())
+app.use(cors())
 
 app.use('/', indexRouter)
 
