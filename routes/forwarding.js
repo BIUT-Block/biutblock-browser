425be99abc1251d5991c9e564fec5c5691b0ade5
Yuan Li <abysmli@gmail.com>
2019-06-11 15:30:47 +0200

    add forwarding api

---
+++ routes/forwarding.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const router = express.Router()
+const request = require('request')
+
+router.get('/', (req, res, next) => {
+  console.log(req.query.url)
+  request({
+    method: 'GET',
+    url: req.query.url,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }, (err, response, body) => {
+    if (err) {
+      res.json(err)
+    }
+    let data = typeof response.body === 'string' ? JSON.parse(response.body) : response.body
+    res.json(data)
+  })
+})
+
+module.exports = router
