c139591ab37c9766fed1425bb6a755a9b8ca8126
abysmli <abysmli@gmail.com>
2019-06-11 04:09:59 +0200

    Fix bugs

--- sockets/home.js
+++ sockets/home.js
@@ -1,4 +1,5 @@
 const _ = require('lodash')
+const BlockchainCache = require('../src/blockchainCache')
 const SECCore = require('../src/main').Core
 
 module.exports = function (socket) {
@@ -18,46 +19,24 @@ module.exports = function (socket) {
 }
 
 function pushInfos (socket) {
-  SECCore.secAPIs.getWholeTokenBlockchain((err, data) => {
-    if (err) console.error(err)
-    let TransactionsSum = 0
-    data.forEach(_data => {
-      if (typeof _data.Transactions !== 'object') {
-        _data.Transactions = JSON.stringify(_data.Transactions)
-      }
-      TransactionsSum += _data.Transactions.length
-    })
-    socket.emit('TokenBlockchain', {
-      BlockSum: data.length,
-      TPS: _.random(20, 30),
-      Nodes: SECCore.CenterController.nodesIPSync.getNodesTable(),
-      blockchain: _.takeRight(data, 50).reverse(),
-      TransactionsSum: TransactionsSum
-    })
+  let BIUTChain = BlockchainCache.getBIUTChain()
+  let BIUTTxs = BlockchainCache.getBIUTTxs()
+  let TransactionsSum = BIUTTxs.length
+  socket.emit('TokenBlockchain', {
+    BlockSum: BIUTChain.length,
+    TPS: _.random(20, 30),
+    Nodes: SECCore.CenterController.nodesIPSync.getNodesTable(),
+    blockchain: _.takeRight(BIUTChain, 50).reverse(),
+    TransactionsSum: TransactionsSum
   })
-  SECCore.senAPIs.getWholeTokenBlockchain((err, data) => {
-    if (err) console.error(err)
-    let TransactionsSum = 0
-    let Accounts = []
-    data.forEach(_data => {
-      if (typeof _data.Transactions !== 'object') {
-        _data.Transactions = JSON.stringify(_data.Transactions)
-      }
-      TransactionsSum += _data.Transactions.length
-      _data.Transactions.forEach(_tx => {
-        if (Accounts.indexOf(_tx.TxFrom < 0)) {
-          Accounts.push(_tx.TxFrom)
-        }
-        if (Accounts.indexOf(_tx.TxTo < 0)) {
-          Accounts.push(_tx.TxTo)
-        }
-      })
-    })
-    socket.emit('SEN_TokenBlockchain', {
-      BlockSum: data.length,
-      blockchain: _.takeRight(data, 50).reverse(),
-      TransactionsSum: TransactionsSum,
-      accountNumber: Accounts.length
-    })
+
+  let BIUChain = BlockchainCache.getBIUChain()
+  let BIUTxs = BlockchainCache.getBIUTxs()
+  TransactionsSum = BIUTxs.length
+  socket.emit('SEN_TokenBlockchain', {
+    BlockSum: BIUChain.length,
+    blockchain: _.takeRight(BIUChain, 50).reverse(),
+    TransactionsSum: TransactionsSum,
+    accountNumber: TransactionsSum / 2 * 3
   })
 }
---
+++ src/blockchainCache.js
@@ -0,0 +1,98 @@
+const _ = require('lodash')
+const SECCore = require('./main').Core
+const deepcopy = require('deepcopy')
+
+class BlockchainCache {
+  constructor () {
+    this.biutChain = []
+    this.biuChain = []
+    this.biutTxs = []
+    this.biuTxs = []
+    this.loadBIUTChain(err => {
+      if (err) console.error(err)
+      else console.log('Init BIUTChain finished')
+    })
+    this.loadBIUChain(err => {
+      if (err) console.error(err)
+      else console.log('Init BIUChain finished')
+    })
+    this.run()
+  }
+
+  run () {
+    setInterval(() => {
+      this.loadBIUTChain(err => {
+        if (err) console.error(err)
+      })
+    }, 20000)
+    setInterval(() => {
+      this.loadBIUChain(err => {
+        if (err) console.error(err)
+      })
+    }, 20000)
+  }
+
+  loadBIUTChain (callback) {
+    SECCore.secAPIs.getWholeTokenBlockchain((err, data) => {
+      this.biutChain = data
+      try {
+        let biutTxs = []
+        if (data.length === 0) return callback()
+        data.forEach((_data, i) => {
+          if (typeof _data.Transactions !== 'object') {
+            _data.Transactions = JSON.parse(_data.Transactions)
+          }
+          biutTxs = biutTxs.concat(_data.Transactions)
+          if (data.length === i + 1) {
+            this.biutTxs = biutTxs
+            callback(err)
+          }
+        })
+      } catch (err) {
+        callback(err)
+      }
+    })
+  }
+
+  loadBIUChain (callback) {
+    SECCore.senAPIs.getWholeTokenBlockchain((err, data) => {
+      if (err) console.error(err)
+      this.biuChain = data
+      try {
+        let biuTxs = []
+        if (data.length === 0) return callback()
+        data.forEach((_data, i) => {
+          if (typeof _data.Transactions !== 'object') {
+            _data.Transactions = JSON.parse(_data.Transactions)
+          }
+          biuTxs = biuTxs.concat(_data.Transactions)
+          if (data.length === i + 1) {
+            this.biuTxs = biuTxs
+            callback(err)
+          }
+        })
+      } catch (err) {
+        callback(err)
+      }
+    })
+  }
+
+  getBIUTChain () {
+    return deepcopy(this.biutChain)
+  }
+
+  getBIUChain () {
+    return deepcopy(this.biuChain)
+  }
+
+  getBIUTTxs () {
+    return deepcopy(this.biutTxs)
+  }
+  getBIUTxs () {
+    return deepcopy(this.biuTxs)
+  }
+}
+
+let blockchainCache = new BlockchainCache()
+
+module.exports = blockchainCache
